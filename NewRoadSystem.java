dd0aca862415eb3e2cb906a776d82968cf605081
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-02-06 12:32:32 -0800

    Add files via upload

---
+++ NewRoadSystem.java
@@ -0,0 +1,34 @@
+package graph;
+
+public class NewRoadSystem {
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    boolean newRoadSystem(boolean[][] roadRegister)
+    {
+        int length = roadRegister.length;
+
+        for (int i = 0; i < length; i++)
+        {
+            int incoming = 0;
+            int outgoing = 0;
+            for (int j = 0; j < length; j++)
+            {
+                if (roadRegister[i][j] == true)
+                    incoming++;
+                if (roadRegister[j][i] == true)
+                    outgoing++;
+            }
+            if (incoming != outgoing)
+                return false;
+
+        }
+        return true;
+
+    }
+
+}
