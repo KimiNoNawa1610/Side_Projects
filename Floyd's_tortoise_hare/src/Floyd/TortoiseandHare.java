36f8c4c12b2a8c3fecbef32d0d3b0c7ee979fa55
Talonss5 <vothanhnhan108@gmail.com>
2020-03-19 22:34:09 -0700

    Update TortoiseandHare.java

---
+++ Floyd's_tortoise_hare/src/Floyd/TortoiseandHare.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Floyd;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author votha
+ */
+public class TortoiseandHare {
+    public int FindDuplication(ArrayList<Integer> nums){
+        int tortoise=nums.get(0);
+        int hare=nums.get(0);
+        boolean condition=true;
+        while(condition){
+            tortoise=nums.get(tortoise);
+            hare=nums.get(nums.get(hare));
+            if(tortoise==hare){
+                condition=false;
+            }
+        }
+        int point1=nums.get(0);
+        int point2=tortoise;
+        while
+    }
+
+    
+    
+}
