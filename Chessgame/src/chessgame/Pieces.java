6e66c60ef814f0adc9538fa4979e6ea0c894fec9
Talonss5 <vothanhnhan108@gmail.com>
2020-03-21 20:05:16 -0700

    Create Pieces.java

---
+++ Chessgame/src/chessgame/Pieces.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sideproject;
+
+/**
+ *
+ * @author votha
+ */
+public abstract class Pieces {
+    public enum pieces{
+        Pawn,Kinght,Rook,Bishop,Queen,King
+    }
+    public enum PiecesType{
+        Black, White
+}
+    public void Pieces(PiecesType, pieces,String x, int y){
+        
+    }
+}
