0e238c959d60e551a51b1fb1c3217dcef752a5f1
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-05-30 22:38:00 -0700

    Update TortoiseandHare.java

--- Floyd's_tortoise_hare/src/Floyd/TortoiseandHare.java
+++ Floyd's_tortoise_hare/src/Floyd/TortoiseandHare.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class TortoiseandHare {
     
     public int FindDuplication(ArrayList<Integer> nums){
+        
         int tortoise=nums.get(0);
         int hare=nums.get(0);
         boolean condition=true;
