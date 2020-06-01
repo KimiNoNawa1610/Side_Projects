b0f8095cb734abc34b149e20721e60097055db3c
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-05-31 21:31:52 -0700

    Update TortoiseandHare.java

--- Floyd's_tortoise_hare/src/Floyd/TortoiseandHare.java
+++ Floyd's_tortoise_hare/src/Floyd/TortoiseandHare.java
@@ -24,6 +24,7 @@ public class TortoiseandHare {
         while(condition){
             
             tortoise=nums.get(tortoise);
+            
             hare=nums.get(nums.get(hare));
             if(tortoise==hare){
                 condition=false;
