57f965d4a65b65bcec6fd815fc19fd711bae4a0e
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-06 22:19:00 -0700

    Update Menu-function.js

--- Mywebsite/Images/Menu-function.js
+++ Mywebsite/Images/Menu-function.js
@@ -4,11 +4,38 @@ const Animation = () => {
 
     const links = document.querySelector('.links');
 
+    const mobilelinks= document.querySelectorAll('.links li');
+
+    //toggle Links
+
     mobile.addEventListener('click',()=>{
 
         links.classList.toggle('active');
 
-    })
+        //Mobile Links Appears
+
+        mobilelinks.forEach((links, indexes) => {
+
+            if (links.style.animation) {
+
+                links.style.animation = "";
+
+            }
+            else {
+
+                links.style.animation = `linksFaded 0.5s ease forwards ${indexes / 5 + 1.2}s`;
+
+            }
+
+        });
+
+    });
+
+    
+
+
+
+
 }
 
 Animation();
