94509e89dcd24e73a277eaf3aa8a50076e0e29a0
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-15 22:12:31 -0700

    Update main.java

--- Enormous Input Test/src/main.java
+++ Enormous Input Test/src/main.java
@@ -1,5 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args){
-        
+    
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String data="";
+
+        File NewFile=new File("D:\\Side_Projects\\Enormous Input Test\\src\\Input.txt");
+
+        Scanner scan=new Scanner(NewFile);
+
+        while(scan.hasNextLine()){
+
+            data=data+" "+scan.nextLine();
+
+        }
+
+        StringBuilder newdata=new StringBuilder(data);
+
+        newdata.deleteCharAt(0);
+
+        data=newdata.toString();
+
+        String[] input=data.split(" ");
+
+        double[] tempin=new double[input.length];
+
+        for(int i=0;i< input.length;i++){
+
+            tempin[i]=Double.parseDouble(input[i]);
+
+        }
+
     }
 }
